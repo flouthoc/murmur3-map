f43522aac48d1dcc236e636e2ace5438d8982dc2
flouthoc <flouthoc.git@gmail.com>
2019-01-16 11:46:59 +0530

    complete

--- murmur3map.go
+++ murmur3map.go
@@ -38,3 +38,76 @@ func NewHashMap(size int) (*HashMap, error){
 	}
 	return h, nil
 }
+
+func (h *HashMap) Get(key string)(*Node, bool){
+	index := h.getIndex(key)
+	//get bucket for the index
+	chain := h.buckets[index]
+	for _, node := range chain{
+		if node.key == key{
+			return &node, true
+		}
+	}
+
+	return nil, false
+}
+
+func (h* HashMap) Set(key string, value interface{}) bool {
+	index := h.getIndex(key)
+	chain := h.buckets[index]
+	found := false
+
+	for i := range chain {
+		node := &chain[i]
+		if( node.key == key{
+			node.Value = value
+			found = true
+		}
+	}
+
+	if found {
+		return true
+	}
+
+	//key is not thee
+	if h.size == h.count {
+		return false;
+	}
+
+	node := Node{key:key, Value: value}
+	chain = append(chain, node)
+	h.buckets[index] = chain
+	h.count++;
+	return true;
+}
+
+
+func (h *HashMap) Delete(key string) (*Node, bool){
+
+	index := h.getIndex(key)
+	chain := h.buckets[index]
+
+	found := false
+	var location int
+	var mapNode *Node
+
+	for loc, node := range chain {
+		if node.key == key {
+			found = true
+			location = loc
+			mapNode = &node
+		}
+	}
+
+	if found {
+		h.count--
+		N := len(chain) - 1
+		chain[location], chain[N] = chain[N], chain[location]
+		chain = chain[:N]
+		h.buckets[index] = chain
+		return mapNode, true
+	}
+
+	return nil, false
+
+}
