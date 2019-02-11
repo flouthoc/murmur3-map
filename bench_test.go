f940b87fa58758f16c34428db7aba6ca5365ffd5
flouthoc <flouthoc.git@gmail.com>
2019-02-11 15:57:17 +0530

    bench test

--- bench_test.go
+++ bench_test.go
@@ -2,19 +2,33 @@ package murmur3map
 
 import ("testing")
 
-func BenchmarkMurmur(b *testing.B){
-
-	var m map[string]int
+	/*var m map[string]int
 	m = make(map[string]int)
 	for n:=0; n<b.N; n++{
 		m["keyfsdfsfsdfsdfds"] = 1
-	}
+	}*/
+
+func BenchmarkMurmur(b *testing.B){
 
 
 
-	/*hashmap,_ := NewMap(1)
+
+	//this will take 16 ns/op
+
+	hashmap,_ := NewMap(1)
 	for n:=0; n<b.N; n++{
 		
 		hashmap.hash("sdfsdfsd")
-	}*/
+	}
+}
+
+func BenchmarkMurmur2(b *testing.B){
+
+	//this will take 32 ns/op
+
+	hashmap,_ := NewMap(1)
+	for n:=0; n<b.N; n++{
+		
+		hashmap.getIndex("sdfsdfsd")
+	}
 }
